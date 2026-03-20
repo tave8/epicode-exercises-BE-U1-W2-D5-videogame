91dcb288dff1258bcb4e57b1fc315b06775a355c
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-20 14:42:12 +0100

    first tests

--- src/main/java/giuseppetavella/programs/test/Main.java
+++ src/main/java/giuseppetavella/programs/test/Main.java
@@ -2,12 +2,19 @@ package giuseppetavella.programs.test;
 
 import giuseppetavella.entities.Collezione;
 import giuseppetavella.entities.Gioco;
+import giuseppetavella.entities.Videogioco;
+import giuseppetavella.enums.GenereVideogioco;
+import giuseppetavella.enums.PiattaformaVideogioco;
 
 public class Main {
     public static void main(String[] args) {
         Collezione<Gioco> giochi1 = new Collezione<>();
+
+        Videogioco videogioco1 = new Videogioco(1, "signore anelli", 10, 1900, 34, PiattaformaVideogioco.PC, GenereVideogioco.SCIFI);
+        Videogioco videogioco2 = new Videogioco(2, "signore anelli", 12, 1900, 34, PiattaformaVideogioco.PC, GenereVideogioco.SCIFI);
         
-        // giochi1.add();
+        giochi1.add(videogioco1);
+        giochi1.add(videogioco2);
         
         giochi1.printStats();
         
